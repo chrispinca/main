1990491f4a4bea188a37c94856dd7c442d1fa163
cpinca <cpinca@sfu.ca>
2024-09-21 18:25:47 -0700

    created the ProductList class to display all products

---
+++ frontend/src/components/ProductList.jsx
@@ -0,0 +1,40 @@
+import ProductCard from "./ProductCard";
+import React, {useState, useEffect} from "react";
+
+function ProductList() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const currentProducts = [
+            { id: 1, name: "Cushy Chair", price: 10.99, image: "https://cb2.scene7.com/is/image/CB2/071124_ViewAll_LivingRoom_Chairs_Filter?wid=520&qlt=75" },
+            { id: 2, name: "Bouncy Bed", price: 12.99, image: "https://www.ikea.com/ca/en/images/products/hauga-bedroom-furniture-set-of-3-lofallet-beige-white__1101338_pe866602_s5.jpg?f=s" },
+            { id: 3, name: "Tall Table", price: 9.99, image: "https://cb2.scene7.com/is/image/CB2/071124_ViewAll_LivingRoom_CoffeeTables_Filter?wid=520&qlt=75" },
+            { id: 4, name: "Cool Chair", price: 12.99, image: "https://specials-images.forbesimg.com/imageserve/63ee96ebff0dadf0a54144a5/Erik-Leather-Wing-Chair---best-furniture-store-online/960x0.jpg?cropX1=0&cropX2=1060&cropY1=0&cropY2=961" },
+            { id: 5, name: "Crazy Couch", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+            { id: 6, name: "Product 6", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+            { id: 7, name: "Product 7", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+            { id: 8, name: "Product 8", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+            { id: 9, name: "Product 9", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+            { id: 10, name: "Product 10", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+        ];
+        setProducts(currentProducts);
+    }, []);
+
+    return (
+        <div className="product-list-container">
+            <div className = "product-list">
+                {products.map(product => (
+                    <ProductCard 
+                        key = {product.id}
+                        image = {product.image}
+                        productName = {product.name}
+                        price = {product.price}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+
+}
+
+export default ProductList;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import Header from "../components/Header";
 import ProductCard from "../components/ProductCard";
+import ProductList from "../components/ProductList";
 
 function Home() {
     return (
         <div>
-            <ProductCard image = "https://cb2.scene7.com/is/image/CB2/071124_ViewAll_LivingRoom_Chairs_Filter?wid=520&qlt=75" productName = "Cushy Chair" price = "10.99"></ProductCard>
+            <h1>Welcome to our store!</h1>
+            <ProductList />
+            
         </div>
     );
 }
