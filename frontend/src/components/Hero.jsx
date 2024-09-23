84fc21415623686083efd78f74e1fedd47f8ee41
cpinca <cpinca@sfu.ca>
2024-09-22 18:38:34 -0700

    updated hero banner to transition between images on an interval

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,13 +1,24 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
+import couchBanner from "../assets/couchBanner.jpg";
 
 function Hero() {
     const slides = [
-        { id: 1, image: "https://wallpapers.com/images/hd/beautiful-tan-modern-furniture-1p8lqahjzvgac2vw.jpg", text: "Welcome to Our Store!" },
+        { id: 1, image: couchBanner, text: "Welcome to Our Store!" },
         { id: 2, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8cXVybai9OXkKrbsBAimT1v5qZIzqJpYGfDwGPCWoggl_QIn_16sImzEjlJRzG9kR2yk&usqp=CAU", text: "Amazing Deals Await!" },
         { id: 3, image: "https://img.freepik.com/premium-photo/interior-dark-living-room-with-black-walls-comfortable-black-sofa_140916-49396.jpg", text: "Shop the Latest Trends!" },
     ];
 
+    const intervalId = useRef(null);
+
     const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+        intervalId.current = setInterval(nextSlide, 6500);
+
+        return () => {
+            clearInterval(intervalId.current);
+        };
+    }, []);
 
     const nextSlide = () => {
         setCurrentSlide((prev) => (prev + 1) % slides.length);
@@ -16,11 +27,20 @@ function Hero() {
     const prevSlide = () => {
         setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
     };
+    
 
     return (
         <div className = "hero" >
             <div className = "hero-slide">
-                    <img className = "hero-image" src = {slides[currentSlide].image}></img>
+                {slides.map((slide, index) => (
+                        <img 
+                            key={slide.id} 
+                            className={`hero-image ${index === currentSlide ? 'active' : ''}`} 
+                            src={slide.image} 
+                            alt={slide.text}
+                        />
+                    ))}
+                        
                 <div className = "hero-text" >
                     <h2>{slides[currentSlide].text}</h2>
                 </div>
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,5 +1,6 @@
 import ProductCard from "./ProductCard";
 import React, {useState, useEffect} from "react";
+import cushyCouch from "../assets/cushyCouch.jpg"
 
 function ProductList() {
     const [products, setProducts] = useState([]);
@@ -10,7 +11,7 @@ function ProductList() {
             { id: 2, name: "Bouncy Bed", price: 12.99, image: "https://www.ikea.com/ca/en/images/products/hauga-bedroom-furniture-set-of-3-lofallet-beige-white__1101338_pe866602_s5.jpg?f=s" },
             { id: 3, name: "Tall Table", price: 9.99, image: "https://cb2.scene7.com/is/image/CB2/071124_ViewAll_LivingRoom_CoffeeTables_Filter?wid=520&qlt=75" },
             { id: 4, name: "Cool Chair", price: 12.99, image: "https://specials-images.forbesimg.com/imageserve/63ee96ebff0dadf0a54144a5/Erik-Leather-Wing-Chair---best-furniture-store-online/960x0.jpg?cropX1=0&cropX2=1060&cropY1=0&cropY2=961" },
-            { id: 5, name: "Crazy Couch", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
+            { id: 5, name: "Crazy Couch", price: 12.99, image: cushyCouch },
             { id: 6, name: "Product 6", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
             { id: 7, name: "Product 7", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
             { id: 8, name: "Product 8", price: 12.99, image: "https://img.freepik.com/premium-photo/discover-beautiful-elegant-wood-furniture-stunning-furniture-images-included_629704-1400.jpg" },
@@ -22,7 +23,7 @@ function ProductList() {
 
     return (
         <div className="product-list-container">
-            <h2>Products</h2>
+            <h2 className = "product-list-title">Featured Products</h2>
             <div className = "product-list">
                 {products.map(product => (
                     <ProductCard 
