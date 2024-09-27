ab980ecaab7b8d0c9b15bfc322fa49cf3d0d3836
cpinca <cpinca@sfu.ca>
2024-09-26 22:32:45 -0700

    created server.js and established db connection to postgres storeDB

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { Pool } from 'postgres-pool';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.connect()
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.error("Database connection error:", err.stack);
+    });
+
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(200).send("Welcome to my online store app!");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
